2ecc72449282f2b2f94b185222619c7162ebaa71
Raoul ALCALA RUIZ <r.alcalaruiz@student.helmo.be>
2020-02-28 10:57:51 +0100

    Correction docs

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management','percent_field'],
+    'depends': ['base','sale_management','percent_field','stock'],
 
     # always loaded
     'data': [
