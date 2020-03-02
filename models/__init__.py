53a59dcfbb2268bec31ae05a3345f8bf6e2a352f
Raoul Alcala Ruiz <alcalaruizraoul@gmail.com>
2020-03-02 14:40:28 +0100

    Fixed fail merge

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import sale_order
+from . import reports_delivery_in_time
+from . import reports
+from . import reports_employee
+from . import yearly_goal
+from . import monthly_goal
+from . import monthly_goal_employee
--- models/user.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class Partner(models.Model):
-    _inherit = 'res.user'
-
-    # Add a new column to the res.partner model, by default partners are not
-    # instructors
-    commercial = fields.Boolean("Commercial", default=False)
